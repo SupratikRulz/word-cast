2aaf8f6039281f5e085ef1099b9e88be0045b9d3
Supratik <supratik@fusioncharts.com>
2019-01-11 00:05:49 +0530

    Added methods to set the font and color palette for font

--- src/wordcast.js
+++ src/wordcast.js
@@ -10,8 +10,10 @@ class WordCast {
   constructor(rootElement = null, wordList = []) {
     this.rootElement = rootElement;
     this.wordList = wordList;
+    this.colorPalette = ['#000000'];
+    this.minFontSize = 8;
+    this.maxFontSize = 16;
   }
-
 
   /**
    * Function to set the root element inside which
@@ -26,7 +28,6 @@ class WordCast {
     return this;
   }
 
-
   /**
    * Function which accepts array of words(strings)
    * and stores them in list.
@@ -38,5 +39,54 @@ class WordCast {
   setWordList(wordList = []) {
     this.wordList = wordList;
     return this;
+  }
+
+  /**
+   * Function to set the minimum font-size
+   * 
+   * @param {number} [minFontSize=8] 
+   * @returns instance of this class
+   * 
+   * @memberOf WordCast
+   */
+  setMinimumFontSize(minFontSize = 8) {
+    this.minFontSize = minFontSize;
+    return this;
+  }
+
+  /**
+   * Function to set the maximum font-size
+   * 
+   * @param {number} [maxFontSize=16] 
+   * @returns instance of this class
+   * 
+   * @memberOf WordCast
+   */
+  setMaximumFontSize(maxFontSize = 16) {
+    this.maxFontSize = maxFontSize;
+    return this;
+  }
+
+  /**
+   * Function to set the color palette for the words to be displayed
+   * 
+   * @param {array} [colorPalette=['#000000']] 
+   * @returns instance of this class
+   * 
+   * @memberOf WordCast
+   */
+  setColorPalette(colorPalette = ['#000000']) {
+    this.colorPalette = colorPalette;
+    return this;
+  }
+
+  /**
+   * Function to generate the word cloud
+   * and append to the DOM
+   * 
+   * @memberOf WordCast
+   */
+  build() {
+    
   }
 }
