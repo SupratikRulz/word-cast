7450556a1293d2908e816be7d47843a2376fb193
Supratik <supratik@fusioncharts.com>
2019-01-10 23:31:36 +0530

    Added method to set root element

--- src/wordcast.js
+++ src/wordcast.js
@@ -1,9 +1,28 @@
 class WordCast {
 
+  /**
+   * Creates an instance of WordCast.
+   * @param {element} [rootElement=null] 
+   * @param {array} [wordList=[]] 
+   * 
+   * @memberOf WordCast
+   */
   constructor(rootElement = null, wordList = []) {
     this.rootElement = rootElement;
     this.wordList = wordList;
   }
 
-  
+
+  /**
+   * Function to set the root element inside which
+   * words will be created.
+   * @param {element} [rootElement=null] 
+   * @returns instance of this class
+   * 
+   * @memberOf WordCast
+   */
+  setRootElement(rootElement = null) {
+    this.rootElement = rootElement;
+    return this;
+  }
 }
